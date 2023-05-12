1593f37cc7d0aca47b2aa5314a958d3be05424c8
i.kasian <illia.kasian@quarks.tech>
2023-05-12 19:27:41 +0300

    add gpt

--- app.py
+++ app.py
@@ -2,22 +2,38 @@ from flask import Flask, render_template, request, jsonify, redirect
 import subprocess
 import sys
 import json
+import os
+import openai
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
+
+openai.api_key = os.getenv('OPENAI_API_KEY')
 
 
 @app.route('/')
 def index():
-    return redirect('/1')
+    # list throught all files from problems folder and get the json data
+    problems = []
+    for filename in os.listdir('problems'):
+        with open(f'problems/{filename}') as f:
+            data = json.load(f)
+            data['id'] = filename.split('.')[0]
+            problems.append(data)
+
+    return render_template('index.html', problems=problems)
 
 
-@app.route('/<problem_id>')
+@app.route('/problem/<problem_id>')
 def problem(problem_id: int):
     # get json data from file from problems folder
     with open(f'problems/{problem_id}.json') as f:
         data = json.load(f)
+        data['id'] = problem_id
 
-    return render_template('index.html', problem=data)
+    return render_template('problem.html', problem=data)
 
 
 @app.route('/submit_solution', methods=['POST'])
@@ -51,6 +67,30 @@ def submit_solution():
 
     # Return the results
     return jsonify(results)
+
+
+@app.route('/api/chat', methods=['POST'])
+def chat():
+    chat_history = request.json.get('history')
+
+    problem_id = request.json.get('problem_id')
+    with open(f'problems/{problem_id}.json') as f:
+        problem = json.load(f)
+
+    user_code = request.json.get('code')
+
+    print(chat_history)
+    # Generate a response using GPT-3 chat models
+    response = openai.ChatCompletion.create(
+        model="gpt-4",
+        messages=chat_history
+    )
+
+    # Get the assistant's reply from the response
+    assistant_reply = response['choices'][0]['message']['content']
+
+    # Return the response
+    return jsonify({'reply': assistant_reply})
 
 
 # run app on port 7777
